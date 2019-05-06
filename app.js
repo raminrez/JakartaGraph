44dcbc8e57603713059350132b0f5dc1f97d4731
Ramin <ramin12207@gmail.com>
2019-05-06 12:36:57 +0430

    Auth FIX

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const isAuth = require("./middleware/is-auth");
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -26,7 +36,7 @@ mongoose
     `mongodb://admin:A9PupM8w4ST1Qcrv@SG-mymongo-20699.servers.mongodirector.com:27017/admin`
   )
   .then(() => {
-    app.listen(3001);
+    app.listen(8000);
     // console.log("App Connected");
   })
   .catch(e => {
